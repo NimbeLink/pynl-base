56a05bda877eb3fe0533275f98bcbc5f8bffc549
Erik Johnson <joh06937@gmail.com>
2021-05-03 09:25:32 -0500

    Source: Added a few more __init__.py files for creating a better package experience

---
+++ nimbelink/__init__.py
@@ -0,0 +1,23 @@
+"""
+The NimbeLink package
+
+(C) NimbeLink Corp. 2020
+
+All rights reserved except as explicitly granted in the license agreement
+between NimbeLink Corp. and the designated licensee. No other use or disclosure
+of this software is permitted. Portions of this software may be subject to third
+party license terms as specified in this software, and such portions are
+excluded from the preceding copyright notice of NimbeLink Corp.
+"""
+
+import nimbelink.cell as cell
+import nimbelink.config as config
+import nimbelink.ui as ui
+import nimbelink.utils as utils
+
+__all__ = [
+    "cell",
+    "config",
+    "ui",
+    "utils"
+]
