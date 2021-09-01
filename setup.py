0fe8992017f8fde828416784d03efd95e4251734
Erik Johnson <erik.johnson@nimbelink.com>
2021-09-01 11:40:30 -0500

    Setup: Bumped version to v1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import find_packages
 setup(
     name = "pynl-base",
     description = "NimbeLink library base",
-    version = "1.0.0",
+    version = "1.0.1",
     packages = find_packages(),
     entry_points = {
         "console_scripts":
