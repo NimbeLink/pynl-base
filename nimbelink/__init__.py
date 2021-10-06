3644c97708073810c79db8494287659d83a549c8
Erik Johnson <erik.johnson@nimbelink.com>
2021-10-06 15:50:18 -0500

    Source: Removed the 'module' subsystem altogether
    Sure which I had managed to randomly stumble upon this earlier, or that my
    Googling for something off-the-shelf for handling this had been more fruitful...

--- nimbelink/__init__.py
+++ nimbelink/__init__.py
@@ -16,61 +16,6 @@ __all__ = [
     "config",
     "debugger",
     "git",
-    "module",
     "ui",
     "utils"
 ]
-
-import importlib
-import sys
-
-class PynlFinder(importlib.abc.MetaPathFinder):
-    """A submodule finder for loading dynamic NimbeLink modules
-    """
-
-    def find_spec(self, fullname, path, target = None):
-        """Finds pynl-like submodule specs
-
-        :param self:
-            Self
-        :param fullname:
-            The name of the package to try to find
-        :param path:
-            The path being sought
-        :param target:
-            The target
-
-        :return None:
-            Failed to find module
-        """
-
-        packages = fullname.split(".")
-
-        # If they aren't looking for a 'nimbelink'-scoped package, this must not
-        # be for a pynl package
-        if packages[0] != "nimbelink":
-            return None
-
-        # If this isn't for a first-level pynl package, we won't be able to load
-        # whatever this is for them
-        #
-        # In this case, we likely loaded the pynl package but now the user is
-        # trying to access something within the (loaded) package that doesn't
-        # exist.
-        if len(packages) != 2:
-            return None
-
-        import nimbelink.module
-
-        # Clean up any dangling modules that no longer exist
-        nimbelink.module.__modules__.prune()
-
-        for module in nimbelink.module.__modules__:
-            # If this is the module they're trying to find, great, found it
-            if module.alias == packages[1]:
-                return module.doImport()
-
-        return None
-
-# Add our submodule finder to the system
-sys.meta_path.append(PynlFinder())
